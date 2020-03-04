f1901054367ca698c092d041dd5f09a66d0ecd64
jamesnitz <jamesanitz2@gmail.com>
2020-03-04 11:20:05 -0600

    urban planner 1 complete

---
+++ Building.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace Planner
+
+{
+  public class Building
+  {
+    private string designer { get; set; }
+
+    private DateTime dateConstructed { get; set; }
+
+    private string Address { get; set; }
+
+    private string Owner { get; set; }
+
+    public int Stories { get; set; }
+    public double Width { get; set; }
+    public double Depth { get; set; }
+    public double Volume
+    {
+      get
+      {
+        return Width * Depth * (3 * Stories);
+      }
+    }
+
+    public Building(string address)
+    {
+      Address = address;
+    }
+
+    public void Construct()
+    {
+      dateConstructed = DateTime.Now;
+    }
+    public void Purchase(string name)
+    {
+      Owner = name;
+    }
+
+
+    public override string ToString()
+    {
+      return ($"Address: {Address}, constructed on {dateConstructed}, and owned by: {Owner}, with a size of {Volume} ");
+    }
+
+  }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Planner
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("This my buildings!!");
+      Building NSS = new Building("301 Plus Park")
+      {
+        Width = 250,
+        Depth = 300,
+        Stories = 5
+      };
+      Building oldNSS = new Building("500 Interstate Blvd");
+      Building dadsHouse = new Building("Spring Hill");
+
+      oldNSS.Width = 250;
+      oldNSS.Depth = 300;
+      oldNSS.Stories = 5;
+
+      dadsHouse.Width = 250;
+      dadsHouse.Depth = 300;
+      dadsHouse.Stories = 5;
+
+      NSS.Construct();
+      oldNSS.Construct();
+      dadsHouse.Construct();
+
+      NSS.Purchase("John Wark");
+      oldNSS.Purchase("James Nitz");
+      dadsHouse.Purchase("The Bank");
+
+      Console.WriteLine(NSS.ToString());
+      Console.WriteLine(oldNSS.ToString());
+      Console.WriteLine(dadsHouse.ToString());
+
+
+    }
+  }
+}
