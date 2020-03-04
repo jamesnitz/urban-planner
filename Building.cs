6ad8f777f87a3733ea0b06c31f517fd79bb8c31f
jamesnitz <jamesanitz2@gmail.com>
2020-03-04 12:52:17 -0600

    phase 2 done

--- Building.cs
+++ Building.cs
@@ -1,18 +1,14 @@
 using System;
 
 namespace Planner
-
 {
   public class Building
   {
     private string designer { get; set; }
-
     private DateTime dateConstructed { get; set; }
-
-    private string Address { get; set; }
-
+    public string Address { get; set; }
+    public string Name { get; set; }
     private string Owner { get; set; }
-
     public int Stories { get; set; }
     public double Width { get; set; }
     public double Depth { get; set; }
@@ -23,12 +19,10 @@ namespace Planner
         return Width * Depth * (3 * Stories);
       }
     }
-
     public Building(string address)
     {
       Address = address;
     }
-
     public void Construct()
     {
       dateConstructed = DateTime.Now;
@@ -37,14 +31,9 @@ namespace Planner
     {
       Owner = name;
     }
-
-
     public override string ToString()
     {
       return ($"Address: {Address}, constructed on {dateConstructed}, and owned by: {Owner}, with a size of {Volume} ");
     }
-
   }
-
-
 }
